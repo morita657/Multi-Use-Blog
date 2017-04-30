632b89db124b80ca738d6e2521a1a1b3c743e9d4
Takahiro Morita <morita657@gmail.com>
2017-04-30 15:04:40 +0700

    Decorate comment form

--- handlers/newcomment.py
+++ handlers/newcomment.py
@@ -4,18 +4,24 @@ from utils import *
 
 class NewComment(BlogHandler):
     def post(self, post_id):
-        comments = self.request.get('comment')
+        comment = self.request.get('comment')
         key = ndb.Key('Post', int(post_id), parent=blog_key())
         post = key.get()
+        comments = Comment.query().filter(Comment.post_id == int(post_id)).order(-Comment.created)
 
         if self.user == "":
             self.redirect('/login')
         elif self.user and self.user.key.id():
-            author = self.user.key.id()
-            post_id = post.key.id()
-            p = Comment(parent = blog_key(), comments=comments, post_id=int(post_id), comment_author=str(self.user.name), author=ndb.Key('User', int(author)))
-            p.put()
-            self.redirect('/blog/%s' % int(p.post_id))
+            if not comment:
+                commenterror = "Please fill in the comment form."
+                post._render_text = post.content.replace('\n', '<br>')
+                self.render('permalink.html', commenterror=commenterror, post=post, comments=comments)
+            else:
+                author = self.user.key.id()
+                post_id = post.key.id()
+                p = Comment(parent = blog_key(), comments=comment, post_id=int(post_id), comment_author=str(self.user.name), author=ndb.Key('User', int(author)))
+                p.put()
+                self.redirect('/blog/%s' % int(p.post_id))
         else:
             error= "You cannot comment on your post"
             self.render('error.html', error=error)
